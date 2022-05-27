fcaa23b59c963338c127e0cca04cf91a06eac1b1
RojaliniDas <100181657+RojaliniDas@users.noreply.github.com>
2022-05-27 20:22:56 +0530

    react-todo

---
+++ src/component/Todo.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import TodoItem from './Todoitem'
+
+export const Todo = () => {
+
+    const [input, setInput]= useState("");
+    const [todo,setTodo]= useState([]);
+
+    const handleChange =(e) =>{
+        setInput(e.target.value)
+    }
+
+    const addTodo = () =>{
+        const obj={
+            title:input
+        }
+        setTodo([...todo,obj])
+    }
+  return (
+    <>
+    <input type="text" placeholder="Write Something" value={input} onChange={handleChange}/>
+    <button onClick={addTodo}>ADD</button>
+    <div>
+        {
+            todo.map((item)=>{
+                return <TodoItem {...item}/>
+    })}
+    </div>
+    </>
+  )
+}
---
+++ src/component/Todoitem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Todoitem(Item) {
+  return (
+    <>
+    <h2>{Item.title}</h2>
+    </>
+  )
+}
+
+export default Todoitem
